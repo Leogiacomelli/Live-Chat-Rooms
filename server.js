ac0edc1e87ea9de1ee084c588174bc82837e44a1
Leandro Giacomelli <leandroegiacomelli@gmail.com>
2023-06-07 14:35:26 -0300

    new updates

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-const botName = "ChatCord Bot";
+const botName = "LiveChat Bot";
 
 /* (async () => {
   pubClient = createClient({ url: "redis://127.0.0.1:6379" });
